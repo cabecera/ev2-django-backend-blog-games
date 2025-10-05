6efc01a440f0c202b7e72a41964b13994da5ab65
cabecera <ci.becerrad@outlook.cl>
2025-10-05 14:27:34 -0300

    feat: Se agrega seccion galeria (html, css, urls, views)

--- bloggames/urls.py
+++ bloggames/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
 
 from core import views as views_core
 from comunidad import views as views_comunidad
@@ -23,12 +24,15 @@ from contacto import views as views_contacto
 from galeria import views as views_galeria
 from noticias import views as views_noticias
 
+def redirect_gallery(request):
+    return redirect('/galeria/', permanent=True)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views_core.home,name="index"),
     # path('comunidad/',views_comunidad.comunidad,name="comunidad"),
     # path('contacto/',views_contacto.contacto,name="contacto"),
-    # path('galeria/',views_galeria.galeria,name="galeria"),
+    path('galeria/', include('galeria.urls')),
+    path('gallery/', redirect_gallery, name="redirect_gallery"),  # Redirección para /gallery/
     path('noticias/',views_noticias.noticias,name="noticias"),
 ]
