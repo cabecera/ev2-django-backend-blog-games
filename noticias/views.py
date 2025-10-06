091e02a9d6d491d75f412ad0d085d4387f77403e
cabecera <ci.becerrad@outlook.cl>
2025-10-05 23:36:55 -0300

    feat: se agrega funcionalidad de paginacion para noticias segun requerimientos

--- noticias/views.py
+++ noticias/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Noticia
 
-# Create your views here.
 def noticias(request):
-    noticias = Noticia.objects.all()
-    return render(request, 'noticias/noticias.html', {'noticias': noticias})
-#Esto obtiene todas las noticias de la base de datos y las envía al template noticias.html.
+    lista_noticias = Noticia.objects.all().order_by('-created')  # Orden descendente por fecha
+    paginator = Paginator(lista_noticias, 5)  # 5 noticias por página
 
+    page_number = request.GET.get('page')  # Captura el número de página de la URL
+    page_obj = paginator.get_page(page_number)
 
+    return render(request, 'noticias/noticias.html', {'page_obj': page_obj})
